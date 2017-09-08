8a5d27a3260886c8d38a1e1649aebf0b7823afa5
WeNdy12340 <31665140+WeNdy12340@users.noreply.github.com>
2017-09-07 22:16:47 -0400

    Add files via upload

---
+++ Homework 1/QuestionFour.java
@@ -0,0 +1,15 @@
+//Wendy Mei
+//CISC 3150
+//HW 1 Question 4
+//went over in class before
+import java.util.*;
+
+public class QuestionFour{
+	public static void main(String [] args){
+		Scanner input= new Scanner(System.in);
+		while(input.hasNextInt()){
+			System.out.println(input.nextInt());
+		}
+		
+	}
+}
